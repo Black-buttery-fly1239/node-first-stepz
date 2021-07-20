46686bee6cacd1e5377d948f36577c18875915ce
nobu-hope <nobuhlelebz@gmail.com>
2021-07-20 13:31:59 +0200

    update

--- figlet.js
+++ figlet.js
@@ -1,10 +1,3 @@
-const chalk = require('chalk');
-
-const greet = require('./greet');
-
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
-
 var figlet = require('figlet');
  
 figlet('Hello World!!', function(err, data) {
